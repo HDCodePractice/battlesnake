3af1e9469d803615fd8e91e626299d0e2c4b60f3
Noah <59535872+SSnipro@users.noreply.github.com>
2021-09-18 10:36:44 -0400

    Create noah.js

---
+++ ai-p/noah.js
@@ -0,0 +1,25 @@
+function noah_getDirection(gridSize,snake,apples,direction){
+    return direction;
+}
+
+function keyPressed() {
+    if (keyCode === LEFT_ARROW && snake[1] != snake[0] - 1) {
+            direction = "l";
+    } else if (keyCode === RIGHT_ARROW && snake[1] != snake[0] + 1) {
+        direction = "r";
+    } else if (keyCode === UP_ARROW && snake[1] != snake[0] - gridSize) {
+        direction = "u";
+    } else if (keyCode === DOWN_ARROW && snake[1] != snake[0] + gridSize) {
+        direction = "d";
+    }
+}
+
+function moreUpdate(){
+
+}
+
+function moreSettings(){
+    
+}
+
+ais['noah'] = noah_getDirection;
