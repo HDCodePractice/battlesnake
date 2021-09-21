a90efa31a9df1cf78adbfbebe5a1635b5b654ae3
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-09-20 17:23:26 -0700

    lol

--- ai-p/jaden.js
+++ ai-p/jaden.js
@@ -1,5 +1,25 @@
 function jaden_getDirection(gridSize,snake,apples,direction){
-    return direction;
+    if (direction == "l") {
+        if (snake[0]%gridSize==0){
+            return "d";
+        }
+        return "l";
+    }else if (direction == "d") {
+        if (snake[0]%gridSize==0){
+            return "r";
+        }
+        return "d";
+    }else if (direction == "r") {
+        if (snake[0]%gridSize==gridSize-1){
+            return "u";
+        }else{
+            return "r";
+        }
+    }else if (direction == "u") {
+        if (snake[0]%gridSize==gridSize-1){
+            return "l";
+        }
+    }
 }
 
 function jaden_newGame(){
