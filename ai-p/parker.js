12eea9c7dbc21063d45cc993522d144f06a43c26
ParkerXChen <parkerxchen@gmail.com>
2021-09-22 19:04:40 -0400

    Add parker AI snake

---
+++ ai-p/parker.js
@@ -0,0 +1,12 @@
+function parker_getDirection(gridSize,snake,apples,direction){
+    
+}
+
+function parker_newGame(){
+    return;
+}
+
+ais['parker'] = {
+    getDirection: parker_getDirection,
+    newGame: parker_newGame
+};
