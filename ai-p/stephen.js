4ea5adc1cc266cc39bdd1ebbcda30e975aeb192b
stephenzhu01 <82696093+stephenzhu01@users.noreply.github.com>
2021-09-27 09:18:41 +0800

    第一阶段
    初步避开吃到自己。

---
+++ ai-p/stephen.js
@@ -0,0 +1,48 @@
+
+
+function stephen_getDirection(gridSize,snake,apples,direction){
+    let apple = indexToColRow(apples[0])
+    let snakeHead = indexToColRow(snake[0])
+    if (apple[0] < snakeHead[0]){
+        if (snake.includes(snake[0] - gridSize)){
+            if (direction === "r"){
+                return "r"
+            }
+            return "l";
+        }
+        return "u"
+    }else if (apple[0] > snakeHead[0]){
+        if (snake.includes(snake[0] + gridSize)){
+            if (direction === "r"){
+                return "r"
+            }
+            return "l";
+        }
+        return "d"
+    }else if (apple[1] < snakeHead[1]){
+        if (snake.includes(snake[0] - 1)){
+            if (direction === "u"){
+                return "u"
+            }
+                return "d"
+        }
+        return "l"
+    }else if (apple[1] > snakeHead[1]){
+        if (snake.includes(snake[0] + 1)){
+            if (direction === "u"){
+                return "u"
+            }
+                return "d"
+        }
+        return "r"
+    }
+}
+
+    function stephen_newGame(){
+    return;
+}
+
+ais['stephen'] = {
+    getDirection: stephen_getDirection,
+    newGame: stephen_newGame
+};
